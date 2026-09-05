f28be942dd2927659446f76005a4945c56448fa7
Rishit <rishitsahay16@gmail.com>
2026-09-05 14:48:43 +0530

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def relu(x) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    x=np.asarray(x,dtype=float)
+    ans=np.maximum(0.0,x)
+    return np.array(ans)
+    pass
